f56088675b1b2682ceb26e40a269190cee058dbb
ghao6735 <Ghao6735@gmail.com>
2026-06-11 10:24:48 +0700

    them loi chuc

---
+++ js/data.js
@@ -0,0 +1,98 @@
+const people = {
+
+    "thái như": {
+
+        name: "Thái Như",
+
+        message:
+            "Chúc Thái Như 20/10 nhận thật nhiều bó hoa từ bạn bè thân thiết 🌷",
+
+        caption:
+            "Hình từ ngày 1 tháng 11 (vì làm mất file rùi)",
+
+        photos: [
+            "assets/TNhw.jpg"
+        ]
+
+    },
+
+    "cô viên-ila": {
+
+        name: "Teacher Viên and teacher Ila",
+
+        message:
+            "Happy Vietnamese Women's Day! Thank you for being not just a great teacher, but also a fun friend in our class",
+
+        caption:
+            "Một vài khoảnh khắc đáng nhớ 🌷",
+
+        photos: [
+     "assets/Cô Viên - Ila 2.jpg"
+        ]
+
+    },
+
+    "cbo": {
+
+        name: "Chj Po",
+
+        message:
+            "Chúc chj 20/10 vui vẻ nheeee 🌷🌷🌷",
+
+       photos: [
+   "assets/CPO4.JPG"
+]
+
+    },
+"xen": {
+
+    name: "Xuân An",
+
+    message:
+        "Chúc Xuân An 20/10 vui vẻ nheee 🌷",
+
+    photos: [
+    
+        "assets/XEN6.JPG"
+
+    ]
+
+},
+
+   "hương tiên": {
+
+        name: "Hương Tiên",
+
+        message:
+            "Chúc Hương Tiên 20/10 vui vẻ nheeee 🌷🌷🌷",
+
+       photos: [
+   "assets/HT6.JPG"
+]
+
+    
+    },
+"kim tiền": {
+
+    name: "Kim Tiền",
+
+    message:
+        "Chúc Kim Tiền 20/10 vui vẻ nheee 🌷",
+        
+    photos: [
+        "assets/KT.JPG" 
+    ]
+    
+},
+
+"dngoc": {
+
+    name: "Diễm Ngọc",
+    message:
+        "Chúc Diễm Ngọc 20/10 vui vẻ nheee 🌷" ,
+    photos: [
+        "assets/DNGOC.JPG"
+    ]
+    
+}
+};
--- js/script.js
+++ js/script.js
@@ -3,48 +3,66 @@ const btn = document.getElementById("confirmBtn");
 const result = document.getElementById("result");
 const devPanel = document.getElementById("devPanel");
 
-const DEV_PASSWORD = "DEV2024";
-
-const data = {
-    "hao": {
-        message: "Chúc bạn có một ngày thật vui vẻ ❤️"
-    }
-};
+const DEV_PASSWORD = "devmode@hao";
 
 function login(){
 
-    const value = input.value.trim().toLowerCase();
+    const value =
+        input.value.trim().toLowerCase();
 
     if(value === DEV_PASSWORD.toLowerCase()){
 
         devPanel.style.display = "block";
 
+        result.innerHTML =
+            "<h3>Xin chào DEV</h3>";
+
+        return;d
+    }
+
+    if(people[value]){
+
+        const person = people[value];
+
+        document.getElementById("mainCard")
+            .style.width = "900px";
+
         result.innerHTML = `
-            <h3>Xin chào DEV 👑</h3>
+
+            <h2>${person.message}</h2>
+
+            <div class="mac-window">
+
+                <div class="mac-bar">
+
+                    <span class="red"></span>
+                    <span class="yellow"></span>
+                    <span class="green"></span>
+
+                </div>
+
+                <img
+                    id="slideImage"
+                    src="${person.photos[0]}">
+
+            </div>
+
+            <p class="caption">
+                ${person.caption}
+            </p>
+
         `;
 
         return;
     }
 
-    if(data[value]){
-
-        result.innerHTML = `
-            <h2>${data[value].message}</h2>
-        `;
-
-        document.querySelector(".card").style.width = "700px";
-
-        return;
-    }
-
-    result.innerHTML = `
-        <p>Không tìm thấy dữ liệu 😥</p>
-    `;
+    result.innerHTML =
+        "<p>Không tìm thấy dữ liệu 😥</p>";
 }
 
 btn.addEventListener("click", login);
 
-input.addEventListener("keydown", (e)=>{
+input.addEventListener("keydown",(e)=>{
 
     if(e.key==="Enter"){
         login();
@@ -54,15 +72,17 @@ input.addEventListener("keydown", (e)=>{
 
 for(let i=0;i<20;i++){
 
-    const flower=document.createElement("div");
+    const flower =
+        document.createElement("div");
 
-    flower.className="flower";
+    flower.className = "flower";
 
-    flower.innerHTML="🌸";
+    flower.innerHTML = "🌸";
 
-    flower.style.left=Math.random()*100+"vw";
+    flower.style.left =
+        Math.random()*100+"vw";
 
-    flower.style.animationDuration=
+    flower.style.animationDuration =
         8+Math.random()*10+"s";
 
     document.body.appendChild(flower);
