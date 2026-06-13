953d771f7801fd2ef9e16ef5b9ff8092f42c2d5e
ghao6735 <Ghao6735@gmail.com>
2026-06-13 22:34:10 +0700

    Cap nhat web 20-10

--- js/data.js
+++ js/data.js
@@ -49,7 +49,7 @@ const people = {
     name: "Xuân An",
 
     message:
-        "Chúc Xuân An 20/10 vui vẻ nheee 🌷",
+        "❤️ Chúc Xuân An 20/10 vui vẻ nheee 🌷 ❤️",
 
     photos: [
     
@@ -64,7 +64,7 @@ const people = {
         name: "Hương Tiên",
 
         message:
-            "Chúc Hương Tiên 20/10 vui vẻ nheeee 🌷🌷🌷",
+            "Chúc Hương Tiên 20/10 vui vẻ nheeee 🌷🌷",
 
        photos: [
    "assets/HT6.JPG"
@@ -77,7 +77,7 @@ const people = {
     name: "Kim Tiền",
 
     message:
-        "Chúc Kim Tiền 20/10 vui vẻ nheee 🌷",
+        "Chúc Kim Tiền 20/10 vui vẻ nheee 🌷 ",
         
     photos: [
         "assets/KT.JPG" 
@@ -98,14 +98,236 @@ const people = {
 
 "tire": {
 
-    name: "Lớp xe",
+    name: "Lốp xe",
 
     message:
-        "Hả! cái lốp mà đòi 20/10 hả =)) thôi thì chúc ông sớm kiếm được người yêu nhaa =)))",
+        "Hả! cái lốp mà đòi 20/10 hả =)) thôi thì chúc bồ sớm kiếm được người yêu nhaa =)))",
 
     photos: [
-        "assets/lốp xe.jpg"
+        "assets/lốp xe.JPG"
     ]
 
-}
+},
+
+"siri": {
+
+    name: "Ngọc Li",
+
+    message:
+        "chúc Ngọc Li 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/siri.jpg"
+    ]
+
+},
+
+"k9 kim ngân": {
+
+    name: "Chị Ngân",
+
+    message:
+        "chúc chị Ngân 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/K9 Kim Ngân.jpg"
+    ]
+
+},
+
+"k9 kim anh": {
+
+    name: "Chị Kim Anh",
+
+    message:
+        "chúc chị Kim Anh 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"ngọc huyền": {
+
+    name: "Ngọc Huyền",
+
+    message:
+        "chúc Ngoc Huyền 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/NGKT.jpg"
+    ]
+
+},
+
+"kim thảo": {
+
+    name: "Kim Thảo",
+
+    message:
+        "chúc Kim Thảo 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/NGKT.jpg"
+    ]
+
+},
+
+"đình": {
+
+    name: "Ngân Đình",
+
+    message:
+        "chúc Ngân Đình 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"phương hoa": {
+
+    name: "Phương Hoa",
+
+    message:
+        "chúc Phương Hoa 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png  "
+    ]
+
+},
+
+"nnhw": {
+
+    name: "Ngọc Như",
+
+    message:
+        "chúc Ngoc Như 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/TVNNHW.jpg"
+    ]
+
+},
+
+"kngoc": {
+
+    name: "Khánh Ngọc",
+
+    message:
+        "chúc Khánh Ngọc 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"trvy": {
+
+    name: "Triệu Vy",
+
+    message:
+        "chúc Triệu Vy 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"dquynh": {
+
+    name: "Diễm Quỳnh",
+
+    message:
+        "chúc Diễm Quỳnh 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"tiên": {
+
+    name: "Hải Tiên",
+
+    message:
+        "chúc Hải Tiên 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"tgvy": {
+
+    name: "Tường Vy",
+
+    message:
+        "chúc Tường Vy 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"kdi": {
+
+    name: "Khánh Vy",
+
+    message:
+        "chúc Khánh Vy 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"tvan": {
+
+    name: "Thùy Vân",
+
+    message:
+        "chúc Thùy Vân 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"hn1m9": {
+
+    name: "Hàn Nguyền mún cao 1m9",
+
+    message:
+        "chúc Hàn Nguyền mún cao 1m9 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
+"athuu": {
+
+    name: "Anh Thư",
+
+    message:
+        "chúc Anh Thư 20/10 vui vẻ nheee 🌷" ,
+
+    photos: [
+        "assets/no img.png"
+    ]
+
+},
+
 };
